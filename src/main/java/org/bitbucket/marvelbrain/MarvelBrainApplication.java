4cf3b9641be5505369c445bfe961a31a8ad28764
Pavan Shankar Koli <pskoli93@gmail.com>
2017-11-16 23:11:33 +0530

    Initial project setup

---
+++ src/main/java/org/bitbucket/marvelbrain/MarvelBrainApplication.java
@@ -0,0 +1,12 @@
+package org.bitbucket.marvelbrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarvelBrainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarvelBrainApplication.class, args);
+	}
+}
